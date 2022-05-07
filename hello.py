cbe7d6a2301bba09bc5ccf75d8562c2c272f8ef0
miya-38-a <85498974+miya-38-a@users.noreply.github.com>
2022-05-08 01:30:06 +0900

    fix hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,26 @@
 import random
 from tkinter.font import ROMAN
 
+# 関数定義の練習
+def FizzBuzz(sum_number :int) -> str:
+    if sum_number%3 == 0 and sum_number%5 == 0:
+        return "FizzBuzz"
+    elif sum_number%3 == 0:
+        return "Fizz"
+    elif sum_number%5 == 0:
+        return "Buzz"
+    else:
+        """
+         型アノテーションはしているが
+         intのまま返してもエラーにはならない
+         (その場合普通にint型になる)
+        """
+        return str(sum_number)
+
 def main():
     print("Hello World!")
 
+    # 要素数5で初期化するがlist型は可変長配列
     list_number = [0 for _ in range(5)]
 
     for i in range(5):
@@ -11,14 +28,7 @@ def main():
     
     sum_number = sum(list_number)
 
-    if sum_number%3 == 0 and sum_number%5 == 0:
-        res = "FizzBuzz"
-    elif sum_number%3 == 0:
-        res = "Fizz"
-    elif sum_number%5 == 0:
-        res = "Buzz"
-    else:
-        res = sum_number
+    res = FizzBuzz(sum_number)
 
     print(res)
 
